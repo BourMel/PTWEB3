007183fafbd596d4bbd1d9f55f1cc447c2e2cc2e
Méline Bour-Lang <meline.bour-lang@etu.unistra.fr>
2016-11-02 09:31:23 +0100

    Revert "Nettoyage du dossier"
    This reverts commit 7d4114e1319a283dd00caeb7bfe11191a3434a69.

--- template/video.php
+++ template/video.php
@@ -8,6 +8,7 @@
 	<link rel="icon" href="favicon.ico" />
     <!--Introduction des feuilles de style-->
     <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/font-awesome.min.css">
 	<link rel="stylesheet" href="./css/flickity.css">
 	<link rel="stylesheet" href="./css/grille.css">
     <link rel="stylesheet" href="./css/style.css">
